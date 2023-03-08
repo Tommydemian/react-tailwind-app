2c1f337be067fde2c61accf38aeef3e758e46204
Tomas <tomas@getcharly.com>
2023-03-08 14:58:18 -0300

    first staructure done

---
+++ README.md
@@ -0,0 +1,2 @@
+1. Tailiwind installed.
+2. 
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import styles from './style';
+
+import {
+    Billing, 
+    Business, 
+    CardDeal, 
+    Clients, 
+    Hero,
+    CTA, 
+    Footer,
+    Navbar, 
+    Stats, 
+    Testimonials
+} from './components';
 
 const App = () => {
   return (
+    
     <div className='bg-primary w-full overflow-hidden'>
-      App
+      <div className={`${styles.paddingX}, ${styles.flexCenter}`}> 
+        <div className={styles.container}>
+          <Navbar />
+        </div>
+      </div>
+    
+    <div className={`bg-primary, ${styles.flexStart}`}>
+      <div className={styles.container}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary, ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={styles.container}>
+        <Stats />
+        <Business/>
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer /> 
+      </div>
+    </div>   
+
     </div>
   )
 }
